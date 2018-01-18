15926bceb9057803c67b16d5a15466c469a03894
chelseaju <chelseaju@ucla.edu>
2018-01-18 15:20:10 -0800

    adding code for MSBWT

---
+++ MSBWT/README.md
@@ -0,0 +1,31 @@
+## Description
+
+[MSBWT](https://github.com/holtjma/msbwt)[1] is designed to compress raw reads via a multi-string variant of Burrows-Wheeler Transform (BWT). The compressed and searchable representation allows counting the occurrences of any *k*-mers. Instead of concatenating all reads and sorting, MSBWT builds a BWT on each string and merges these multi-string BWTs through a small interleave array. The final structure allows a fast query of *k*-mers of arbitrary *k*. 
+
+We build the multi-string BWTs on all reads. This compressed representation is stored in files, and later loaded in memory for frequency retrieval. It is one of the implementations that does not require scanning through reads one *k* at a time.
+
+### for short reads
+```
+bin/msbwt cffq --uniform --compressed -p 1 tmpdir readfile
+```
+### for long reads
+```
+bin/msbwt cffq -p 1 tmpdir readfile 
+```
+
+## To run:
+```
+python msbwt.py signatures readfile outfile tmpdir
+	signature = list of kmers
+	readfile = fasta or fastq
+	outfile = name of outfile
+	tmpdir = temporary directory to store files, and will be delted by the end of the program
+```
+
+## To record the memory footprint:
+```
+mprof run -C  python msbwt.py signatures readfile outfile tmpdir
+```
+
+### Reference:
+[1]: J. Holt and L. McMillan.  Merging of multi-string BWTs with applications.Bioinformatics, 30(24):3524–31, dec2014
---
+++ MSBWT/msbwt.py
@@ -0,0 +1,59 @@
+"""
+	Function: Given a read file, and a list of kmer, return the kmer frequency 
+	To Run:	
+		python msbwt.py signatures readfile outfile tmpdir  
+	Author: Chelsea Ju
+	Date: 2017-12-31
+
+"""		
+import sys, os, argparse, datetime, time
+MSBWT="bin/msbwt"
+
+def echo(msg):
+        print "[%s] %s" % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), str(msg))
+
+def main(parser):
+	option = parser.parse_args()
+	readfile = option.readfile
+	kmer = option.signature
+	tmpdir = option.tmpdir
+	outfile = option.outfile
+
+	start_time = time.time()
+
+	echo("Indexing Reads")
+	if not os.path.exists(tmpdir):
+   		os.makedirs(tmpdir)
+
+	## for short reads
+	## CMD: bin/msbwt cffq --uniform --compressed -p 1 tmpdir readfile
+	## for long reads 
+	## CMD: bin/msbwt cffq -p 1 tmpdir readfile 
+	os.system("%s cffq --uniform --compressed -p 1 %s %s" %(MSBWT, tmpdir, readfile))
+
+	echo("Querying Kmers")
+	tmpfile = tmpdir + "/outfile.txt"
+	
+	## CMD: bin/msbwt massquery --rev-comp tmpdir signature tmpfile
+	os.system("%s massquery --rev-comp %s %s %s" %(MSBWT, tmpdir, kmer, tmpfile))
+	os.system("awk -F',' '{print $1\"\\t\"$2+$3}' %s > %s" %(tmpfile, outfile))
+	
+	#echo("Cleaning")
+	#os.system("rm -rf %s" %(tmpdir))
+
+	echo("Done")
+	print('Total runtime: %s seconds' %(time.time() - start_time))
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(prog="msbwt.py")
+	parser.add_argument("signature", type=str, help="list of kmers")
+	parser.add_argument("readfile", type=str, help="sequencing read file")
+	parser.add_argument("outfile", type=str, help="output file")
+	parser.add_argument("tmpdir", type=str, help="temporary directory")
+
+#	parser.add_argument("-i", "--read", dest="readfile", type=str, help="sequencing read file", required = True)
+#	parser.add_argument("-s", "--kmer", dest="kmer", type=str, help="list of kmers", required = True)
+#	parser.add_argument("-t", "--tmpdir", dest="tmpdir", type=str, help="temporary directory", required = True)
+#	parser.add_argument("-o", "--outfile", dest="outfile", type=str, help="outfile", required = True)
+  	main(parser)	
+
